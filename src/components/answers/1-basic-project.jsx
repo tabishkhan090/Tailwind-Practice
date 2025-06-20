867d4669be6e2501514a53954a0571fdfd572740
MD TABISH <tabishrazakhan92@gmail.com>
2025-06-21 02:34:07 +0530

    During class

---
+++ src/components/answers/1-basic-project.jsx
@@ -0,0 +1,12 @@
+// sm, md, lg, xl, 2xl
+
+export function SidebarClass1() {
+    return <div className="flex">
+        <div className="transition-all ease-in-out duration-150 md:w-96  h-screen w-0 ">
+            Sidebar
+        </div>
+        <div className="bg-green-800 h-screen flex-1">
+            Content
+        </div>
+    </div>
+}
